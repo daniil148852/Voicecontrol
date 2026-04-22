6e2c4bd70543ed151fa942b328122194a04ec817
daniil148852 <daniildan850@gmail.com>
2026-04-22 17:31:06 +0200

    Update VolumeIndicator.hpp

--- src/VolumeIndicator.hpp
+++ src/VolumeIndicator.hpp
@@ -2,15 +2,21 @@
 
 #include <Geode/Geode.hpp>
 
+// Must come after Geode.hpp so cocos2d types are available.
+using namespace geode::prelude;
+
 namespace voicecontrol {
-    class VolumeIndicator : public CCNode {
+
+    class VolumeIndicator : public cocos2d::CCNode {
     public:
         static VolumeIndicator* create();
+
         bool init() override;
         void update(float dt) override;
 
     private:
-        CCLayerColor* m_background = nullptr;
-        CCLayerColor* m_fill = nullptr;
+        cocos2d::CCLayerColor* m_background = nullptr;
+        cocos2d::CCLayerColor* m_fill       = nullptr;
     };
-}
+
+} // namespace voicecontrol
